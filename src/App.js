2225e2dfea4ef5d8a5d40c9853a9011092ea8979
blenhenok <79223969+blenhenok@users.noreply.github.com>
2024-09-29 18:49:12 +0300

    nav bar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+    const [menu,setmenu] = useState("shop");
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt='' />
+            <p>SHOPPER</p>
+        </div>
+        <ul className="nav-menu">
+            <li onClick={() =>{setmenu("shop")}}> Shop {menu === "shop" ? <hr/> : <></>} </li>
+            <li onClick={() =>{setmenu("womens")}}>Women {menu === "womens" ? <hr/> : <></>}</li>
+            <li onClick={() =>{setmenu("mens")}}>Men {menu === "mens" ? <hr/> : <></>}</li>
+            <li onClick={() =>{setmenu("kids")}}>Kids {menu === "kids" ? <hr/> : <></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt='' />
+            <p className='nav-cart-count'>0</p>
+        </div>
+    </div>
   )
 }
 
